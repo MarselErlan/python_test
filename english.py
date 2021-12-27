3af88db08ff2fa76a35d9388eb7a5cddcc7ef53b
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2021-12-27 09:28:16 +0300

    print 5

---
+++ english.py
@@ -0,0 +1,179 @@
+slovar = {
+    "Attire": "наряд",
+    "Flow": "течение",
+    "Threaten": "угрожать",
+    "Occupied": "занятый",
+    "Balanced": "уравновешенный",
+    "Species": "разновидность",
+    "Enlarge": "увеличивать",
+    "Droll": "забавный",
+    "Landscape": "пейзаж",
+    "Cruel": "жестокий",
+    "Sluggish": "медлительный",
+    "Half": "половина",
+    "Evening": "вечерный",
+    "Between": "между",
+    "Climb": "взбираться",
+    "Wheel": "колесо",
+    "Shoelace": "шнурок",
+    "Overeat": "переедать",
+    "Nurse": "медсестра",
+    "Guest": "гость",
+    "Harmless": "безобидный",
+    "Awkward": "неловкий",
+    "Soothe": "успокаивать",
+    "Disappoint": "разочаровывать",
+    "Heavy": "тяжелый",
+    "Secluded": "уединенный",
+    "Wooden": "деревянный",
+    "Transmute": "превращать",
+    "Disobedient": "непослушный",
+    "Skull": "череп",
+    "Common": "общий",
+    "Wire": "проволока",
+    "Funeral": "похороны",
+    "Sibling": "кровные братья",
+    "Coffin": "гроб",
+    "Benefits": "выгоды",
+    "Advantages": "преимущества",
+    "Wraith": "призрак",
+    "Wealth": "богатство",
+    "Pervert": "извращать",
+
+
+
+}
+
+
+right = 0
+wrong = 0
+from random import choice
+running = 1
+while running:
+    k = [value for value in slovar.values()]
+    s = choice(k)
+    print(s)
+    testing = input('перевод? : ')
+    if testing in slovar:
+        value1 = slovar[testing]
+        if s == value1:
+            right += 1
+            print("правильных ответов")
+            print(right)
+            print(f'правилно! {testing} означает  : ' + value1)
+
+        elif s != value1:
+            wrong += 1
+            print("неправильных ответов")
+            print(wrong)
+            print("попробуйту еще раз!")
+
+    else:
+        print(slovar.get("fwefe", "такого слова нет в словаре, попробуйту еще раз! "))
+
+    if testing == "1":
+        break
+
+#     if comp > user:
+#         print("вы проиграли!")
+#         cash -= bet
+#         print(cash)
+#         with open("results.txt", "a") as file:
+#             file.write(f"\nк:{comp}, ю:{user} ставка: {bet} проиграш! остаток: {cash}")
+#     elif comp < user:
+#         print("вы победили!")
+#         cash += bet
+#         print(cash)
+#         with open("results.txt", "a") as file:
+#             file.write(f"\nк:{comp}, ю:{user} ставка: {bet} победа! остаток: {cash}")
+#     else:
+#         print("ничья")
+#         print(cash)
+#         with open("results.txt", "a") as file:
+#             file.write(f"\nк:{comp}, ю:{user} ставка: {bet} ничья!:  остаток: {cash}")
+# with open("results.txt", "a") as file:
+#     file.write(f"\nвремя игры: {datetime.now() - start}")
+
+# from random import choice
+# k = [value for value in slovar.values()]
+# s = k.pop(k.index(choice(k)))
+# print(s)
+#
+# prob = input('перевод? : ')
+# testing = prob
+#
+# if testing in slovar:
+#     value1 = slovar[testing]
+#     if s == value1:
+#         print('провилно!  : ' + value1)
+# else:
+#     print(slovar.get("fwefe", "попробуйту еще раз!"))
+
+
+
+
+
+
+# f = open('my_file.txt', 'w')
+# f.write(slovar[value])
+
+
+# print(slovar.get("fwefe", "нет токого слова!"))
+# try:
+# except KeyError:
+#     print()
+
+# print(slovar["hello"])
+
+#
+# from random import choice
+# from datetime import  datetime
+# from time import  sleep
+#
+# start = datetime.now()
+# print(start)
+# sleep(0.000000000000001)
+# print((datetime.now() - start).seconds)
+# print(datetime.now())
+#
+# #                таймер
+# # def timer (seconds):
+# #     while seconds != 0:
+# #         sleep(1)
+# #         print(seconds)
+# #         seconds -= 1
+# # timer(10)
+#
+#
+#
+# asked = []
+# students = [2, 3, 4, 5, 8, 12, 13, 14, 15, 16, 17, 18, 19, 21]
+#
+# while len(students) != 0:
+#     student = students.pop(students.index(choice(students)))
+#     answer = input(f'отвечает студент под номером {student} ')
+#     asked.append(student)
+#     print(students)
+#     print(asked)
+#
+#
+# # from joji7_1 import greeting as g
+# # import random
+# # from random import randint, choice, sample
+# # from random import randrange as r
+# #             вывод из списка
+# # names = ['azat', 'ruslan', 'medina']
+# # g(choice(names))
+#
+#
+# # print(r(1, 10, 4))
+# # print(r(1, 10, 4))
+#
+#
+# # lst = ['a', 'b', 'c', 'd']
+# # print(sample(lst, 3))
+# # print(choice(lst))
+# # print(random.choice(lst))
+#
+# # print(random.randint(1, 5))
+# # print(randint(1, 5))
