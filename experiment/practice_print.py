c1719d311f9dce4548dda0a6f2e3eab035364162
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2021-12-30 23:06:32 +0300

    practice_printing_1day

---
+++ experiment/practice_print.py
@@ -0,0 +1,127 @@
+# практика печатание - practice printing
+"""
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+import imoprt import import import import import import import import import import import import import
+import import import import import import import import import import import import import import import
+#
+
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print(' print('
+
+
+#
+
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+while while while while while while while while while while while while while while while while while
+
+
+#
+
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+name name name name name name name name name name name name name name name name name name name name name
+
+
+#
+
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
+
+
+#
+
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elfi elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif elif
+
+
+#
+
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: elSe: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else: else:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
