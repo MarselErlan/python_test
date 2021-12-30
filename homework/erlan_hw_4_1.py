e348e717b4406e52b3e784f7c44cdacfbb9f959b
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2021-12-30 11:01:40 +0300

    version2

--- homework/erlan_hw_4_1.py
+++ homework/erlan_hw_4_1.py
@@ -1,21 +1,35 @@
+# class TimeDesk():
+#     def __init__(self, second):
+#         self.second = second
+#
+#
+# def seconds_to_dhms(time):
+#     seconds_to_minute = 60
+#     seconds_to_hour = 60 * seconds_to_minute
+#     seconds_to_day = 24 * seconds_to_hour
+#     days = time // seconds_to_day
+#     time %= seconds_to_day
+#     hours = time // seconds_to_hour
+#     time %= seconds_to_hour
+#     minutes = time // seconds_to_minute
+#     time %= seconds_to_minute
+#     seconds = time
+#     print("%d days, %d hours, %d minutes, %d seconds" % (days, hours, minutes, seconds))
+#
+#
+# time = int(input("Enter the number of seconds: "))
+# seconds_to_dhms(time)
+
+
+
 class TimeDesk():
-    def __init__(self, second):
-        self.second = second
+    def sayHi(self):
+        second = int(input())
+        minutes = second // 60
+        hours = minutes // 60
+        days = hours // 24
+        print(days, hours, minutes,)
 
 
-def seconds_to_dhms(time):
-    seconds_to_minute = 60
-    seconds_to_hour = 60 * seconds_to_minute
-    seconds_to_day = 24 * seconds_to_hour
-    days = time // seconds_to_day
-    time %= seconds_to_day
-    hours = time // seconds_to_hour
-    time %= seconds_to_hour
-    minutes = time // seconds_to_minute
-    time %= seconds_to_minute
-    seconds = time
-    print("%d days, %d hours, %d minutes, %d seconds" % (days, hours, minutes, seconds))
-
-
-time = int(input("Enter the number of seconds: "))
-seconds_to_dhms(time)
+p = TimeDesk()
+p.sayHi()
