3a101566547d109b7bc18c933eb0dd2f290c99a6
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2022-07-12 14:58:49 +0600

    practice_touch_typing_fighting_22

--- english_grama/e_words_copy_1.py
+++ english_grama/e_words_copy_1.py
@@ -4,13 +4,19 @@ words = 0
 running = 1
 while running:
     e_for_choice = choice(list(e_slovar100.slovar.items()))
-    enter2 = f'                                                           *                    {e_for_choice[0]}  \n'
+    enter2 = f'               {e_for_choice[1]}                              *                    {e_for_choice[0]}  \n'
     enter3 = (e_for_choice[0])
     enter = input(enter2)
     if enter3 == enter:
         pass
         words += 1
         print('                                                                                      ', words)
+        with open(r'/Users/macbookpro/PycharmProjects/python_test/english_grama/e_slovar100.py',
+                  'a') as f:
+            f.write(f'#    "{e_for_choice[0]}": "{e_for_choice[1]}",\n')
+            f.close()
+        e_slovar100.slovar.pop(e_for_choice[0])
+        print("  \n  ")
 
     elif enter == "1":
         pass
